a24b3e93ec8916153288f9bb4cae0ae8136dc665
gabryel <gabryelaraujo2019@gmail.com>
2026-05-26 16:34:15 -0300

    refactor: aplica tipagem User nos dados mock de usuários

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,14 +1,16 @@
-export const users = [
+import type { User } from "../types";
+
+export const users: User[] = [
     {
         id: "1",
         nome: "Maria",
         saldo: 50,
-        senha: "123"
+        senha: "123" // TODO: hash com bcrypt antes de produção
     },
     {
         id: "2",
         nome: "Pedro",
         saldo: 30,
-        senha: "1234"
+        senha: "1234" // TODO: hash com bcrypt antes de produção
     }
 ];
