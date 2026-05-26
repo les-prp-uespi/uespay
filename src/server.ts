c8c00476555bdf047525aa08c7c55f30e072a043
gabryel <gabryelaraujo2019@gmail.com>
2026-05-26 16:35:45 -0300

    fix: usa process.env.PORT ao invés de porta hardcoded

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,14 @@ import "dotenv/config";
 import saldoRoutes from "./routes/saldo.routes";
 import transacoesRoutes from "./routes/transacoes.routes";
 
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use("/api/saldo", saldoRoutes);
-
 app.use("/api/transacoes", transacoesRoutes);
 
-app.listen(3000, () => {
-    console.log(`Servidor rodando na porta 3000`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
