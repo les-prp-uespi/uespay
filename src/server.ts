dc3cc3e38e00d8e723c7cb95c968418fc7b0fed4
gabryel <gabryelaraujo2019@gmail.com>
2026-05-26 17:01:33 -0300

    feat: adiciona CORS ao servidor Express

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import saldoRoutes from "./routes/saldo.routes";
 import transacoesRoutes from "./routes/transacoes.routes";
@@ -6,6 +7,7 @@ import transacoesRoutes from "./routes/transacoes.routes";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/saldo", saldoRoutes);
