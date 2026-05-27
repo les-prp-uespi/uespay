5ad241a79b39cc9125f58c934965dd353bb7f336
gabryel <gabryelaraujo2019@gmail.com>
2026-05-26 23:02:09 -0300

    feat: registra rota /api/usuarios no servidor Express

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import saldoRoutes from "./routes/saldo.routes";
 import transacoesRoutes from "./routes/transacoes.routes";
+import usuariosRoutes from "./routes/usuarios.routes";
 import { errorHandler } from "./middlewares/error.middleware";
 import { inicializarTokenPool } from "./services/firefly.service";
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/saldo", saldoRoutes);
 app.use("/api/transacoes", transacoesRoutes);
 
